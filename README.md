623c2aa1ad36a80e490a1084dc9ac3967bab0a81
aatango <80591203+aatango@users.noreply.github.com>
2021-09-02 21:42:16 +0200

    time booking tool; 1st issue

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Library
 Collection of general scripts and utilities that could be used across multiple projects.
+
+### Bit Flag Menu
+Aimed to implement filter from Rhynoscript's GetObjects method (https://developer.rhino3d.com/api/RhinoScriptSyntax/#collapse-GetObjects).
+Could be used to implement menus in CLI applications, et al.
+
+### Timebooker
+Small utility to help me keep track of time.
+Task name; time spent on it; and any comments are written to file for later processing.
+This file is meant to be deleted, and restarted, at arbritrary times; usually a work day.
---
+++ timebooker.py
@@ -0,0 +1,48 @@
+"""Small utility to help me keep track of time spent per task.
+
+Upon launching, will calculate time spent using info written on last line.
+Afterwards, asks for user inputs: project number, and comments.
+Finally, writes the whole existing data[1], replacing the last line
+Appends time of writing at the end of file, for future use.
+
+If no file exists, or is badly formatted, a new log is started.
+
+[1] File is not expected to be big enough to affect performance.
+"""
+
+# to be formatted as function if ever needed
+if __name__ == '__main__':
+	import time
+
+	
+	FILE_PATH = '../'
+	FILE_NAME = 'timekeeper'
+	TIME_APPROX = 0.25 #Hours
+	try:
+		with open(FILE_PATH + FILE_NAME, 'r') as file:
+			lines = file.readlines()
+		start_time = float(lines[-1])
+		current_time = time.time()
+		# approx by ceiling to interval; flooring also possible
+		# -(-a//b) == math.ceil(a)
+		spent_time = -(-(current_time - start_time)/3600//TIME_APPROX)\
+			*TIME_APPROX
+		print('| NEW ENTRY |')
+		project = input('Project name: ')
+		comment = input('Comments: ')
+		last_line = '\t'.join(
+			[time.ctime(start_time), f'{spent_time:.2f}', project, comment]
+		)
+
+		new_lines = lines[:-1] + [last_line] + ['\n'+ str(time.time())]
+		with open(FILE_PATH + FILE_NAME, 'w') as file:
+			file.writelines(new_lines)
+		# if print below is done then entry is surely added
+		print(last_line)
+	except:
+		current_time = time.time()
+		with open(FILE_PATH + FILE_NAME, 'w') as file:
+			file.write(str(current_time))
+		print('New log started:')
+		print(time.ctime(current_time))
+		print(FILE_PATH + FILE_NAME)
